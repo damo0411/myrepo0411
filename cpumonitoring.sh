9d2a4b8c4ced53e5a2e6d1d38b6970f44fb681df
damodaran_b <damodaran951104@gmail.com>
2024-06-22 14:24:10 +0530

    cpu script

--- cpumonitoring.sh
+++ cpumonitoring.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 threshold=90
-cpu_usage=$(top -bn1|grep -i "Cpu(s)"|awk '{print $2}'|cut -d. -f1)
-if [ "$cpu_usage" -gt "$threshold" ];
+cpu_usage=$(top -bn1 | grep -i "Cpu(s)"|awk '{print $2}'|cut -d. -f1)
+
+if [ $cpu_usage -gt $threshold ];
 then
-    echo "CPU usgae is high"
+    echo "CPU usage is not OK"
     echo "$cpu_usage%"
 else
-    echo "Cpu usage is OK"
+    echo "CPU is normal"
 fi
